87b48ca41537a465e07f78fd5c7df2e7217bad29
sara-lopez <sara.lopez@petroprix.com>
2025-03-26 13:09:29 +0100

    feat: [CLIENT]: Crear clase Cliente

---
+++ src/main/java/com/sarawipay/practica/model/Client.java
@@ -0,0 +1,9 @@
+package com.sarawipay.practica.model;
+
+public class Client {
+    private String name;
+    private String surname;
+    private String cifNifNie;
+    private String phone;
+    private String email;
+}
