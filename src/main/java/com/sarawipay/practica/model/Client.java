fd57e4652fd7405488759ec14fb05ce784e20b3a
sara-lopez <sara.lopez@petroprix.com>
2025-03-27 12:13:46 +0100

    feat: [CLIENT]: Actualizar modelo de datos

--- src/main/java/com/sarawipay/practica/model/Client.java
+++ src/main/java/com/sarawipay/practica/model/Client.java
@@ -1,9 +1,30 @@
 package com.sarawipay.practica.model;
 
-public class Client {
-    private String name;
-    private String surname;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@DynamoDBTable(tableName = "MainTable")
+public class Client extends MainTable {
+    @DynamoDBAttribute(attributeName = "cifNifNie")
     private String cifNifNie;
+
+    @DynamoDBAttribute(attributeName = "name")
+    private String name;
+
+    @DynamoDBAttribute(attributeName = "surname")
+    private String surname;
+
+    @DynamoDBAttribute(attributeName = "phone")
     private String phone;
+
+    @DynamoDBAttribute(attributeName = "email") // Va a ser el SK, dato duplicado
     private String email;
+
 }
